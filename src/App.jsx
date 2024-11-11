9402811a786670f51b429b9ae8f9c615f292a25e
arunit11 <146124319+arunit11@users.noreply.github.com>
2024-11-11 13:57:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import About from './pages/About'
 import Skill from './pages/Skill'
 import Portfolio from './pages/Portfolio'
 import Contact from './pages/Contact'
-import Gallery from './pages/Gallery'
 
 function App() {
 
